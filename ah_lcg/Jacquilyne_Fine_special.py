cb3e9ceeb9360e533b93ffd852230108d865bc0a
EgorVjalkov <e.vjalkov@yandex.ru>
2022-07-01 06:24:17 +0300

    add Jacquilyne_Fine_special

--- ah_lcg/Jacquilyne_Fine_special.py
+++ ah_lcg/Jacquilyne_Fine_special.py
@@ -4,30 +4,30 @@ from random import choice
 
 #skill = int(input('Investigator`s skill = '))
 #check = int(input('skill check = '))
-chaos_bag = night_of_the_zealot_normal()
+chaos_bag = night_of_the_zealot_normal_bag_keys
 
 
 def Jacquilyne_Fine_special(pull_tokens):
 #    result = skill - check
     pull_tokens = pull_tokens + 2
-    print(pull_tokens)
+#    print(pull_tokens)
     tokens = []
     for i in range(pull_tokens):
         i = choice(chaos_bag)
         chaos_bag.remove(i)
         tokens.append(i)
-    if not -99 in tokens:
+    if not 'auto_fail' in tokens:
         for i in range(2):
             print(f'Tokens are {tokens}.' )
             cancelled = int(input(f'What token do you cancelled? press num from "1" to "{pull_tokens-i}": '))
             tokens.remove(tokens[cancelled-1])
         return tokens
     else: 
-        tokens = [i for i in tokens if not i == -99]
-        tokens.remove(-99)
+        tokens = [i for i in tokens if not i == 'auto_fail']
+        tokens.remove('auto_fail')
         return tokens
 
-print(Jacquilyne_Fine_special(1))
+print(Jacquilyne_Fine_special(5))
 
 
 #    first = choice(chaos_bag)
