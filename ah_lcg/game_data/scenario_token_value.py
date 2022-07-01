244d86ac9a3fa92e7138fcc1b568c8c95f5bad84
EgorVjalkov <e.vjalkov@yandex.ru>
2022-07-01 06:29:06 +0300

    mod scenario token value and investigators

--- ah_lcg/game_data/scenario_token_value.py
+++ ah_lcg/game_data/scenario_token_value.py
@@ -1,16 +1,33 @@
-#night_of_the_zealot_normal_bag = [
-#    1, 0, 0, -1, -1, -1, -2, -2, -3, -4, 'skull', 'skull', 'cultist', 'tablet', 'Elder_Sign', 'auto_fail'
-#    ]
+# прочитай про мэйн, сделай так, чтобы не собирать все сумки подряд
+#сделай хрень с жетонами, типа там удар на таблетку ну ты понял. 
 
-def night_of_the_zealot_normal():
-    skull = -int(input('Count of ghouls at your location? '))
-    cultist = -1
-    tablet = -2
-    auto_fail = -99
-    Elder_Sign = 1
-    return [
-    1, 0, 0, -1, -1, -1, -2, -2, -3, -4, skull, skull, cultist, tablet, Elder_Sign, auto_fail
-    ]
+#значения жетонов
+night_of_the_zealot_normal_bag = {
+    '+1' : 1, '0': 0, '-1': -1, '-2': -2,
+    '-3': -3,'-4': -4, 'skull': -int(input('Count of ghouls at your location? ')), 
+    'cultist': -1, 'tablet': -2, 'Elder_Sign': 1, 'auto_fail': -99
+}
+#список с ключами от словаря
+night_of_the_zealot_normal_bag_keys = ['+1', '0', '0', '-1', '-1', '-1', '-2', '-2', '-3', '-4', 
+    'skull', 'skull', 'cultist', 'tablet', 'Elder_Sign', 'auto_fail']
+
+#для тестов
+only_auto_fail_bag = {'auto_fail': -99}
+only_auto_fail_bag_keys = ['auto_fail']
+only_Elder_Sign_bag = {'Elder_Sign': 1}
+only_Elder_Sign_bag_keys = ['Elder_Sign']
+
+#print(night_of_the_zealot_normal_bag)
+
+#def night_of_the_zealot_normal():
+#    skull = -int(input('Count of ghouls at your location? '))
+#    cultist = -1
+#    tablet = -2
+#    auto_fail = -99
+#    Elder_Sign = 1
+#    return [
+#    1, 0, 0, -1, -1, -1, -2, -2, -3, -4, skull, skull, cultist, tablet, Elder_Sign, auto_fail
+#    ]
 
 def the_midnigth_masks_normal():
     skull = -int(input('The highest number of doom on a Cultist enemy in play? '))
@@ -35,3 +52,7 @@ def the_devourer_below_normal():
 
 
 #print(the_devourer_below_normal())
+
+def main():
+    if __name__ == '__main__':
+        main()
